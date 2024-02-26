09cf9afa5a12e66979f1e449fad4d305eedc3c80
yogesh-tembe <yogesh.tembe@forcepoint.com>
2024-02-26 12:55:04 +0530

    updated test_page2

---
+++ test_page/scripts.js
@@ -0,0 +1,4 @@
+var a="sometest";
+document.addEventListener("DOMContentLoaded", (event) => {
+  console.log("DOM fully loaded and parsed");
+});
---
+++ test_page/scripts2.js
@@ -0,0 +1,6 @@
+var b=34409;
+var c=0;
+for(var i=0;i<b;i++){
+    c=c+1;
+} 
+console.log(c);
